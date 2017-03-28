6589077e57396e1b82231435c74dcc86dd2d8f18
abhinav <abhi.king11@egmail.com>
2017-03-28 21:50:31 +0530

    First commit

---
+++ app/src/main/java/com/example/abhinav/volley/Rconfig.java
@@ -0,0 +1,13 @@
+package com.example.abhinav.volley;
+
+
+
+public class Rconfig {
+    //Data URL
+    public static final String DATA_URL = "http://abhinav.site11.com/volley/feed.php?page=";
+
+    //JSON TAGS
+    public static final String TAG_IMAGE_URL = "image";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_PUBLISHER = "publisher";
+}
